5eec555825f2653b5ecc72723a3511e0237534af
CathieLC <catherine-le-calve_student2021@wilder.school>
2023-03-30 20:59:02 +0200

    add pieces

--- src/store/views.py
+++ src/store/views.py
@@ -12,6 +12,7 @@ def index(request):
     produits = Product.objects.all()
     return render(request, 'store/index.html', context={"produits":produits})
 
+
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug) # en rouge c'est le champs de la table, et ce qu'on lui passe c'est ce qu'on récupère dans la fonction
     cart = get_object_or_404(Cart, user=request.user)
